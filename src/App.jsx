8c38dcfe20e780d567c4e67c0199d8ba6a50e9dc
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2018-05-08 00:42:48 +0530

    final commit

---
+++ src/App.jsx
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import { FormGroup,FormControl,Button,InputGroup,Glyphicon } from 'react-bootstrap';
+import './App.css';
+import Profile from './Profile.jsx';
+import Gallery from './Gallery.jsx';
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      query:'',
+      artist: null,
+      tracks: [],
+      related: []
+    }
+  }
+
+  search() {
+        console.log('this.state', this.state);
+        const BASE_URL = 'https://api.spotify.com/v1/search?';
+        let FETCH_URL = BASE_URL + 'q=' + this.state.query + '&type=artist&limit=1';
+        const ALBUM_URL = 'https://api.spotify.com/v1/artists/';
+        let accessToken = 'BQD24N3gT0AgsyOgo9H9KQyaVRCZidIZBKiskVHRZcSN_Pbyu8Aui_sCICasoFkD5vcKlE6I6qtqfv2IIupW9fDzPPQf7xKBT4f7NCZ8l_Qut6jEFZo5Z_TquopmgBzcj_FQnoPPeWf4nqszH8mw-hGgWlCGgGjecA&refresh_token=AQBQvD093uU5cCDDlk3ZkQrEAyLoVS7oascF0lS2OPOt3GeIzfctxb5sZ4Dco2ChO0MxS_drblYgmF320UZxc1LX38PE1WLMsbWFYjtMcVU7p9Zs_fmrgKUoW1NykRWU8Bw'
+
+        var myOptions = {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + accessToken
+            },
+            mode: 'cors',
+            cache: 'default'
+        };
+
+        fetch(FETCH_URL, myOptions)
+            .then(response => response.json())
+            .then(json => {
+                const artist = json.artists.items[0];
+                this.setState({ artist });
+
+                FETCH_URL = `${ALBUM_URL}${artist.id}/top-tracks?country=US&`
+                fetch(FETCH_URL, myOptions)
+                .then(response => response.json())
+                .then(json => {
+                    const { tracks } = json;
+                    this.setState({ tracks });
+                })
+            });
+    }
+
+  render(){
+    return(
+      <div className="App">
+        <div className="App-title">MusicMaster</div>
+        <FormGroup>
+          <InputGroup>
+            <FormControl
+              type="text"
+              placeholder="Search for an Artist"
+              value={this.state.query}
+              onChange={event => {this.setState({query: event.target.value})}}
+              onKeyPress={event => {
+                if (event.key === 'Enter') {
+                  this.search()
+                }
+              }}
+            />
+            <InputGroup.Addon class="button" onClick={() => this.search()}>
+              <Glyphicon glyph="search"></Glyphicon>
+            </InputGroup.Addon>
+          </InputGroup>
+        </FormGroup>
+        {
+          this.state.artist !== null
+          ?
+            <div>
+              <Profile
+                artist={this.state.artist}
+              />
+              <Gallery
+                tracks={this.state.tracks}
+              />
+            </div>
+          : <div></div>
+        }
+
+      </div>
+    );
+  }
+}
+
+export default App;
